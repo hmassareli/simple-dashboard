a45849966aef51e79a52e7d65d0a6eaad58eeeab
Filipe Batista <filipe.x@live.com>
2024-08-01 12:05:09 -0300

    Adicionando botão de voltar

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -45,7 +45,7 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb"
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Products() {
   const navigate = useNavigate();
@@ -144,6 +144,12 @@ export default function Products() {
           </Breadcrumb>
         </div>
         <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+          <Link to={"/"}>
+            <Button variant="outline" size="icon" className="h-8 w-8">
+              <ChevronLeftIcon className="h-4 w-4" />
+              <span className="sr-only">Back</span>
+            </Button>
+          </Link>
           <div className="relative ml-auto flex-1 md:grow-0">
             <SearchIcon className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
             <Input
@@ -311,6 +317,24 @@ export default function Products() {
   );
 }
 
+function ChevronLeftIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="m15 18-6-6 6-6" />
+    </svg>
+  );
+}
 
 function CirclePlusIcon(props: React.ComponentProps<"svg">) {
   return (
